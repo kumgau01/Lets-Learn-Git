d5b90c93acfe134377c5ecc0f5c09c0b0a0a9755
Gaurav Kumar <sinhamonu98@gmail.com>
2025-01-04 18:20:22 +0530

    Updated the msg

--- Diff.java
+++ Diff.java
@@ -3,6 +3,6 @@ public class Diff{
         int a = 10;
         int b = 20;
         int res = a-b;
-        System.out.println("res is: "+res);
+        System.out.println("Difference is: "+res);
     }
 }
